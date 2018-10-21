0e9514456b0e7c22f9fe8c481f8b3ab6b03634c2
Pham Dong <pd15dth06@gmail.com>
2018-10-21 22:39:19 +0700

    install express, socket.io

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('Server running port: 3000...');
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+})
+
+io.sockets.on('connection', function(socket){
+    //connect server
+    connections.push(socket);
+    console.log('Connected:   %s sockets connected', connections.length);
+
+    //disconnect
+    socket.on('disconnect', function(data){ //if you are connected will no reload
+        
+        users.splice(users.indexOf(socket.uesrname), 1);
+        updateUsernames();
+        connections.splice(connections.indexOf(socket), 1); 
+        console.log('Disconnected: %s sockets connected', connections.length);
+    });
+
+    //send message
+    socket.on('send message', function(data){
+       
+        io.sockets.emit('new message', {msg: data, user:  socket.uesrname});
+    });
+
+    //new user
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.uesrname = data;
+        users.push(socket.uesrname);
+        updateUsernames();
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users);
+    }
+})
+ 
